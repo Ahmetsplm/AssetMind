d00d7832b1d8de08cbcab9ee609d9ef85979ea55
Ahmet Kemal Şipleme <ahmetsplm@gmail.com>
2026-05-10 23:38:06 +0300

    feat: supabase baglantisi ve dotenv guvenlik altyapisi kuruldu

--- lib/main.dart
+++ lib/main.dart
@@ -9,9 +9,19 @@ import 'providers/market_provider.dart';
 
 import 'theme/app_theme.dart';
 import 'providers/theme_provider.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() async {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  await dotenv.load(fileName: ".env");
+
+  await Supabase.initialize(
+    url: dotenv.env['SUPABASE_URL']!,
+    anonKey: dotenv.env['SUPABASE_ANON_KEY']!,
+  );
+
   runApp(const MyApp());
 }
 
